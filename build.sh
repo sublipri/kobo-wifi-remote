d097d813953b4f67b48a04f2226b1419d3faa453
sublipri <sublipri@proton.me>
2023-08-06 14:47:22 +0800

    Refactor main script out of build script.
    It had become unwieldy to have the main script embedded in the build
    script. This should make life easier and removes shfmt as a build
    dependency.

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ for d in "$UDEV_DIR" "$HTTP_DIR" "$BIN_DIR" "$LICENSE_DIR" "$EVENTS_DIR" "$USER_
 done
 
 cp -r -t ./build/root/"$HTTP_DIR" ./src/www/*
-cp -r -t ./build/root/"$BIN_DIR" ./bin/evemu-*
+cp -r -t ./build/root/"$BIN_DIR" ./bin/evemu-* ./src/wifiremote.sh
 cp -r -t ./build/root/"$LICENSE_DIR" ./bin/licenses/*
 mkdir ./build/root/"$LICENSE_DIR"/wifiremote
 cp -t ./build/root/"$LICENSE_DIR"/wifiremote ./LICENSE
@@ -43,120 +43,26 @@ ln -s -r ./build/root/"$UDEV_FILE" ./build/root/"$UDEV_LINK"
 UNINSTALL_UDEV_RULES="KERNEL==\"eth*\", ACTION==\"add\", RUN+=\"$BIN_DIR/wifiremote uninstall\"
 KERNEL==\"wlan*\", ACTION==\"add\", RUN+=\"$BIN_DIR/wifiremote uninstall\""
 
-# create main script that sets up the environment and manages httpd
+# create wrapper script that sets up the environment and runs main script
 cat >./build/root/"$MAIN_SCRIPT" <<SHELL
 #!/bin/sh
-set -u
 
 export KOBO_CONFIG_FILE="/mnt/onboard/.kobo/Kobo/Kobo eReader.conf"
 export CONFIG_FILE="$USER_DIR/config"
 export PATH="\$PATH:$BIN_DIR:$HTTP_DIR/cgi-bin/lib"
 export HTTP_DIR="$HTTP_DIR"
+export HTTP_PORT="$HTTP_PORT"
 export EVENTS_DIR="$EVENTS_DIR"
 export VERSION="$VERSION"
 export CSV_FILE="$CSV_FILE"
 export DATA_DIR="$DATA_DIR"
 export DIR_LIST="$DIR_LIST"
 export FILE_LIST="$FILE_LIST"
+export UDEV_FILE="$UDEV_FILE"
+export UDEV_LINK="$UDEV_LINK"
 
-syslog_pid=\$(pgrep -o -f syslogd)
-while [ "\$syslog_pid" = "" ]; do
-	sleep 1
-	syslog_pid=\$(pgrep -o -f syslogd)
-done
-
-httpd_pid=""
-httpd_running() {
-	httpd_pid=\$(pgrep -o -f "^/bin/busybox httpd.*\$HTTP_DIR")
-	if [ "\$httpd_pid" != "" ]; then
-		pgrep -laf "^/bin/busybox httpd.*\$HTTP_DIR" | logger -p 7 -t wifiremote-main
-		return 0
-	else
-		return 1
-	fi
-}
-
-start_httpd() {
-	if httpd_running; then
-		echo "Wi-Fi Remote already running"
-	else
-		# If e-reader has just booted log dmesg in case there's useful info.
-		# Over time dmesg will fill up with spam that overwrites anything useful.
-		uptime="\$(cat /proc/uptime)"
-		if [ "\${uptime%%.*}" -lt 60 ]; then
-			dmesg | logger -p 7 -t wifiremote-system
-		fi
-		tr '\0' '\n' <"/proc/\$(pidof -s dbus-daemon)/environ" | sed /^snum/d | logger -p 7 -t wifiremote-system
-		df -h | logger -p 7 -t wifiremote-system
-		logger -p 6 -t wifiremote-main "Starting Wi-Fi Remote $VERSION"
-		printenv | sort | logger -p 7 -t wifiremote-main
-		# unset udev environment variables
-		unset UDEV_LOG ACTION SEQNUM IFINDEX DEVPATH SUBSYSTEM INTERFACE
-		unset OF_NAME OF_FULLNAME OF_COMPATIBLE_0 OF_TYPE OF_FULLNAME OF_COMPATIBLE_N MODALIAS DRIVER
-		/bin/busybox httpd -vv -f -h "$HTTP_DIR" -p "$HTTP_PORT" 2>&1 | logger -p 7 -t wifiremote-httpd &
-	fi
-}
-
-stop_httpd() {
-	if httpd_running; then
-		logger -p 6 -t wifiremote-main "Stopping Wi-Fi Remote"
-		logger -p 7 -t wifiremote-main "Killing PID \$httpd_pid"
-		kill "\$httpd_pid"
-	else
-		echo "Wi-Fi Remote not running"
-	fi
-}
-
-enable_wifiremote() {
-	ln -s "$UDEV_FILE" "$UDEV_LINK"
-	if ! httpd_running; then
-		start_httpd
-	fi
-	echo "Wi-Fi Remote enabled"
-}
-
-disable_wifiremote() {
-	rm "$UDEV_LINK"
-	if httpd_running; then
-		stop_httpd
-	fi
-	echo "Wi-Fi Remote disabled"
-}
-
-cmd="\$1"
-
-if [ "\$cmd" = "start" ]; then
-	start_httpd
-elif [ "\$cmd" = "stop" ]; then
-	stop_httpd
-elif [ "\$cmd" = "disable" ]; then
-	disable_wifiremote
-elif [ "\$cmd" = "enable" ]; then
-	enable_wifiremote
-elif [ "\$cmd" = "toggle" ]; then
-	if httpd_running; then
-		disable_wifiremote
-	else
-		enable_wifiremote
-	fi
-elif [ "\$cmd" = "uninstall" ]; then
-	stop_httpd
-	cd /
-	directories="\$(cat "$DIR_LIST")"
-	while read -r filename; do
-		rm -v "\$filename" 2>&1 | logger -p 6 -t wifiremote-uninstall
-	done <"$FILE_LIST"
-	rm -v "$EVENTS_DIR"/* 2>&1 | logger -p 6 -t wifiremote-uninstall
-	rmdir -v "$HTTP_DIR"/* 2>&1 | logger -p 6 -t wifiremote-uninstall
-	while read -r directory; do
-		rmdir -v "\$directory" 2>&1 | logger -p 6 -t wifiremote-uninstall
-	done <<-DELETE
-	\$directories
-	DELETE
-fi
+wifiremote.sh "\$@"
 SHELL
-# Format and catch any syntax errors in above script
-shfmt --write ./build/root/"$MAIN_SCRIPT"
 chmod +x ./build/root/"$MAIN_SCRIPT"
 
 # create NickelMenu config for toggling wifiremote
---
+++ src/wifiremote.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+# SPDX-FileCopyrightText: 2023 sublipri <sublipri@proton.me>
+# SPDX-License-Identifier: GPL-3.0-only
+# Main script. Must be called by wrapper script that sets up the environment
+set -u
+# Fail early if environment not set up
+echo "$HTTP_DIR" >/dev/null
+
+syslog_pid=$(pgrep -o -f syslogd)
+while [ "$syslog_pid" = "" ]; do
+	logger -s -p 6 -t wifiremote-main "Syslog not running. Waiting 1 second."
+	sleep 1
+	syslog_pid=$(pgrep -o -f syslogd)
+done
+
+httpd_pid=""
+httpd_running() {
+	httpd_pid=$(pgrep -o -f "^/bin/busybox httpd.*$HTTP_DIR")
+	if [ "$httpd_pid" != "" ]; then
+		pgrep -laf "^/bin/busybox httpd.*$HTTP_DIR" | logger -p 7 -t wifiremote-main
+		return 0
+	else
+		return 1
+	fi
+}
+
+start_httpd() {
+	if httpd_running; then
+		echo "Wi-Fi Remote already running"
+	else
+		# If e-reader has just booted log dmesg in case there's useful info.
+		# Over time dmesg will fill up with spam that overwrites anything useful.
+		uptime="$(cat /proc/uptime)"
+		if [ "${uptime%%.*}" -lt 60 ]; then
+			dmesg | logger -p 7 -t wifiremote-system
+		fi
+		tr '\0' '\n' <"/proc/$(pidof -s dbus-daemon)/environ" | sed /^snum/d | logger -p 7 -t wifiremote-system
+		df -h | logger -p 7 -t wifiremote-system
+		logger -p 6 -t wifiremote-main "Starting Wi-Fi Remote 0.1.0"
+		printenv | sort | logger -p 7 -t wifiremote-main
+		# unset unneeded environment variables so they're not logged
+		unset UDEV_LOG ACTION SEQNUM IFINDEX DEVPATH SUBSYSTEM INTERFACE
+		unset OF_NAME OF_FULLNAME OF_COMPATIBLE_0 OF_TYPE OF_FULLNAME OF_COMPATIBLE_N MODALIAS DRIVER
+		unset UDEV_LINK UDEV_FILE
+		/bin/busybox httpd -vv -f -h "$HTTP_DIR" -p "$HTTP_PORT" 2>&1 | logger -p 7 -t wifiremote-httpd &
+	fi
+}
+
+stop_httpd() {
+	if httpd_running; then
+		logger -p 6 -t wifiremote-main "Stopping Wi-Fi Remote"
+		logger -p 7 -t wifiremote-main "Killing PID $httpd_pid"
+		kill "$httpd_pid"
+	else
+		echo "Wi-Fi Remote not running"
+	fi
+}
+
+enable_wifiremote() {
+	ln -s "$UDEV_FILE" "$UDEV_LINK"
+	if ! httpd_running; then
+		start_httpd
+	fi
+	echo "Wi-Fi Remote enabled"
+}
+
+disable_wifiremote() {
+	rm "$UDEV_LINK"
+	if httpd_running; then
+		stop_httpd
+	fi
+	echo "Wi-Fi Remote disabled"
+}
+
+cmd="$1"
+
+if [ "$cmd" = "start" ]; then
+	start_httpd
+elif [ "$cmd" = "stop" ]; then
+	stop_httpd
+elif [ "$cmd" = "disable" ]; then
+	disable_wifiremote
+elif [ "$cmd" = "enable" ]; then
+	enable_wifiremote
+elif [ "$cmd" = "toggle" ]; then
+	if httpd_running; then
+		disable_wifiremote
+	else
+		enable_wifiremote
+	fi
+elif [ "$cmd" = "uninstall" ]; then
+	stop_httpd
+	cd /
+	directories="$(cat "$DIR_LIST")"
+	while read -r filename; do
+		rm -v "$filename" 2>&1 | logger -p 6 -t wifiremote-uninstall
+	done <"$FILE_LIST"
+	rm -v "$EVENTS_DIR"/* 2>&1 | logger -p 6 -t wifiremote-uninstall
+	rmdir -v "$HTTP_DIR"/* 2>&1 | logger -p 6 -t wifiremote-uninstall
+	while read -r directory; do
+		rmdir -v "$directory" 2>&1 | logger -p 6 -t wifiremote-uninstall
+	done <<-DELETE
+		$directories
+	DELETE
+fi
