7a3973cd14b519a2f6cbf3385a916042f8d860cf
sublipri <sublipri@proton.me>
2024-01-31 14:11:43 +0800

    Remove old shell script backend

--- evemu/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Requires about 10GB of free space
-OUTPUT_DIR="${OUTPUT_DIR:-./build}"
-
-mkdir -p "$OUTPUT_DIR"
-sudo docker build --build-arg UID="$(id -u)" --build-arg GID="$(id -g)" --tag=kobo-evemu .
-sudo docker run -v "$OUTPUT_DIR":/home/kobo/ localhost/kobo-evemu
--- src/www/cgi-bin/get-log.cgi
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Content-Type: application/x-download"
-echo "Content-Disposition: attachment; filename=wifiremote-$(date +'%s').log.gz"
-echo ""
-logread | grep wifiremote- | gzip -
