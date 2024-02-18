1847d663962e68cc86991dd37cdab7f6b15e573d
sublipri <sublipri@proton.me>
2024-02-18 08:50:57 +0800

    Migrate action recording frontend to new backend

--- src/www/js/alert-recording.js
+++ src/www/js/alert-recording.js
@@ -3,7 +3,7 @@
 
 const timers = {};
 
-function reset() {
+function reset_alerts() {
   const buttons = document.getElementsByClassName("records-input");
   for (const button of buttons) {
     const modal = document.getElementById(`${button.id}-modal`);
@@ -12,15 +12,17 @@ function reset() {
   }
 }
 
-export function setup(duration = 5) {
-  window.addEventListener("pageshow", reset);
-
-  const buttons = document.getElementsByClassName("records-input");
-  for (const button of buttons) {
+export function setup() {
+  window.addEventListener("pageshow", reset_alerts);
+  const forms = document.querySelectorAll(".action-form");
+  console.log(forms);
+  for (const form of forms) {
+    const button = form.querySelector("button");
+    console.log(button);
     button.onclick = function () {
-      alertRecording(duration, button.id);
+      recordAction(form, button.id);
     };
-    const modal_html = `
+    const alert_recording_modal = `
     <div id="${button.id}-modal" class="modal">
       <div class="modal-content">
         <span id="${button.id}-close-modal" class="close">&times;</span>
@@ -29,7 +31,16 @@ export function setup(duration = 5) {
       </div>
     </div>
 `;
-    document.body.insertAdjacentHTML("beforeend", modal_html);
+    const result_modal = `
+    <div id="result-modal" class="modal">
+      <div class="modal-content">
+        <span id="result-close-modal" class="close">&times;</span>
+        <p></p>
+      </div>
+    </div>
+`;
+    document.body.insertAdjacentHTML("beforeend", alert_recording_modal);
+    document.body.insertAdjacentHTML("beforeend", result_modal);
 
     const close = document.getElementById(`${button.id}-close-modal`);
     close.onclick = function () {
@@ -38,7 +49,14 @@ export function setup(duration = 5) {
       clearInterval(timers[button.id]);
       window.stop();
     };
+    const close_result = document.getElementById("result-close-modal");
+    close_result.onclick = closeResult;
   }
+}
+
+function closeResult() {
+  const modal = document.getElementById("result-modal");
+  modal.style.display = "none";
 }
 
 export function alertRecording(timeleft, id) {
@@ -55,4 +73,58 @@ export function alertRecording(timeleft, id) {
     }
   }, 1000);
   timers[id] = timer;
+}
+
+// Convert HTML form to Javascript object
+function processForm(form) {
+  var data = {};
+  for (const input of form.querySelectorAll("input")) {
+    if (input.value === "") {
+      continue;
+    }
+    if (input.className === "input-number") {
+      data[input.name] = parseInt(input.value, 10);
+    } else if (input.type === "checkbox") {
+      data[input.name] = input.checked;
+    } else {
+      data[input.name] = input.value;
+    }
+  }
+  return data;
+}
+
+export async function recordAction(form, id) {
+  if (!form.checkValidity()) {
+    form.reportValidity();
+    return;
+  }
+  const data = processForm(form);
+  const payload = JSON.stringify(data);
+  const config = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: payload,
+  };
+  alertRecording(data.no_input_timeout / 1000, id);
+  const response = await fetch("/actions", config);
+  reset_alerts();
+  const result = document.getElementById("result-modal");
+  const p = result.querySelector("p");
+  if (response.ok) {
+    const recorded = await response.json();
+    p.innerHTML = `Recorded ${recorded.name} in ${recorded.rotation} rotation`;
+    result.style.display = "block";
+    await sleep(2000);
+    closeResult();
+  } else {
+    p.innerHTML = await response.text();
+    result.style.display = "block";
+  }
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
