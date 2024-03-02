36f7d01a189cf773bb243e6d61cc47e3ce2661eb
sublipri <sublipri@proton.me>
2024-03-03 06:28:03 +0800

    Specify new_event_timeout in record-inputs.sh

--- record-inputs.sh
+++ record-inputs.sh
@@ -2,10 +2,10 @@
 # Useful for testing or as a way to quickly record actions without the web UI
 set -e
 echo "Recording Next Page"
-curl -s --json '{"name": "Next", "sort_value": "%00", "path_segment": "next-page", "keyboard_shortcut": "ArrowRight"}' "$KOBO_HOST"/actions | jq .
+curl -s --json '{"name": "Next", "sort_value": "%00", "path_segment": "next-page", "keyboard_shortcut": "ArrowRight", "new_event_timeout": 100 }' "$KOBO_HOST"/actions | jq .
 echo "Recording Prev Page"
-curl -s --json '{"name": "Prev", "sort_value": "%01", "path_segment": "prev-page", "keyboard_shortcut": "ArrowLeft"}' "$KOBO_HOST"/actions | jq .
+curl -s --json '{"name": "Prev", "sort_value": "%01", "path_segment": "prev-page", "keyboard_shortcut": "ArrowLeft", "new_event_timeout": 100 }' "$KOBO_HOST"/actions | jq .
 echo "Brightness Up"
-curl -s --json '{"name": "🔆", "keyboard_shortcut": "PageUp"}' "$KOBO_HOST"/actions | jq .
+curl -s --json '{"name": "🔆", "keyboard_shortcut": "PageUp", "new_event_timeout": 100 }' "$KOBO_HOST"/actions | jq .
 echo "Brightness Down"
-curl -s --json '{"name": "🔅", "keyboard_shortcut": "PageDown"}' "$KOBO_HOST"/actions | jq .
+curl -s --json '{"name": "🔅", "keyboard_shortcut": "PageDown", "new_event_timeout": 100 }' "$KOBO_HOST"/actions | jq .
